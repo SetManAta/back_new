e2830f0c660186d80dca6b434aaca983dafee25a
SetManAta <karabukaev.e@gmail.com>
2023-01-13 11:55:04 +0600

    first

---
+++ accounts/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    patronymic = models.CharField(max_length=20)
+
+
+class Author(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    registered = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user.username}'
